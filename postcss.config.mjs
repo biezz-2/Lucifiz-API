cfe1a81cbf54bbc3a65b92cb95fb4c67f39ed949
biezz-2 <biezz-2@users.noreply.github.com>
2026-01-31 17:24:58 +0700

    Fix merge conflicts and valid package.json

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
 export default config;
-=======
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-}
-
-export default config
->>>>>>> 5fbbb0b565f181c555ca9b2dd3e5161d22496582
